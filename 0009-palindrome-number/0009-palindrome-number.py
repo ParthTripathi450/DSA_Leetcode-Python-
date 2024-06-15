33f2f0309d0325d6c1f12c297fd7ccd9a434755d
Parth Tripathi <142286635+ParthTripathi450@users.noreply.github.com>
2024-06-15 13:20:35 +0530

    Time: 46 ms (36.79%), Space: 11.7 MB (7.12%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x<0:
+            return False
+        reversed_num=0
+        temp=x
+        while temp!=0:
+            digit=temp%10
+            reversed_num=reversed_num*10+digit
+            temp//=10
+        if reversed_num==x:
+            return True
+
+
+
